21daf24e21c5f61aea78e238322fc697481e18fe
‘chrisfrick’ <108237130+chrisfrick@users.noreply.github.com>
2023-02-01 14:35:53 -0500

    Commit with correct author info

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return (
+    <h1>
+      <p>{props.course}</p>
+    </h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.content[0]} />
+      <Part part={props.content[1]} />
+      <Part part={props.content[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.content.reduce((total, part) => total + part.exercises, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const content = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using props to pass data', exercises: 7},
+    {name: 'State of a component', exercises: 14}
+]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />
+      <Total content={content} />
+    </div>
+  )
+}
+
+export default App
