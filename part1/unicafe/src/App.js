c575aec0516297c296eb4c6e6f10cb332df36954
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-02-04 17:32:20 -0500

    Exercise 1.8 unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,20 +8,34 @@ const Display = ({ name, value }) => (
   <div>{name} {value}</div>
 )
 
+const Statistics = ({ good, neutral, bad}) => {
+   // calculate statistics
+   let all = good + neutral + bad
+   let average = ((good * 1) + (bad * -1))/all
+   let percentPositive = ((good / all) * 100) + "%"
+
+  return (
+    <div>
+      <Display name="good" value={good}/>
+      <Display name="neutral" value={neutral}/>
+      <Display name="bad" value={bad}/>
+      <Display name="all" value={all}/>
+      <Display name="average" value={average}/>
+      <Display name="positive" value={percentPositive}/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
-  const[neutral, setNeutral] = useState(0)
+  const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   // event handlers for button clicks
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
-
-  let all = good + neutral + bad
-  let average = ((good * 1) + (bad * -1))/all
-  let percentPositive = ((good / all) * 100) + "%"
 
   return (
     <div>
@@ -30,12 +44,11 @@ const App = () => {
       <Button text="neutral" handleClick={handleNeutral}/>
       <Button text="bad" handleClick={handleBad}/>
       <h1>statistics</h1>
-      <Display name="good" value={good}/>
-      <Display name="neutral" value={neutral}/>
-      <Display name="bad" value={bad}/>
-      <Display name="all" value={all}/>
-      <Display name="average" value={average}/>
-      <Display name="positive" value={percentPositive}/>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
