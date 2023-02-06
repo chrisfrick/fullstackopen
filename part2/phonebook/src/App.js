c85b693624fbbf93a0dd515d4ad0121f7ab97413
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-02-05 20:46:49 -0500

    Ex 2.8 phonebook step3 user can add numbers along with names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,31 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "123-456-7890" }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const handleNumberChange = (event) =>
+    setNewNumber(event.target.value)
+
+  const addPerson = (event) => {
     event.preventDefault()
     // Need to stringify objects because objects won't compare with ===
     if (JSON.stringify(persons).includes(JSON.stringify(newName))) {
       return alert(`${newName} is already added to the phonebook`)
     }
-    const personObject = { name: newName }
+    const personObject = { name: newName, number: newNumber}
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +43,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
-          <div key={person.name}>{person.name}</div>
+          <div key={person.name}>{person.name} {person.number}</div>
         )}
       </div>
     </div>
