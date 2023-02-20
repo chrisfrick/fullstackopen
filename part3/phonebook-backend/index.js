07c3686528ac2866e0304400d00b918924caabce
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-02-20 00:03:34 -0500

    Phonebook backend move error handling to middleware Change backend so deleting entries is reflected in database Ex 3.15 and 3.16 phonebook database steps 3 and 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(morgan((tokens, req, res) => {
@@ -24,31 +24,6 @@ app.use(morgan((tokens, req, res) => {
   return log.join(' ')
 }))
 
-/*
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-*/
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
       response.json(people)
@@ -58,31 +33,32 @@ app.get('/api/persons', (request, response) => {
 // const entries = persons.length
 
 app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${entries} people</p>
-    <p>${new Date()}</p>
-  `)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+  Person.find({}).then(people => {
+    response.send(`
+      <p>Phonebook has info for ${people.length} people</p>
+      <p>${new Date()}</p>
+    `)
   })
-  /*
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-  */
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -113,6 +89,16 @@ app.post('/api/persons', (request, response) => {
 
 app.use(morgan(':data'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
