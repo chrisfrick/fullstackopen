eb27b92602dfc8a234677e89b64fcd8e8ef7bd9d
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-02-26 16:45:34 -0500

    Added favoriteBlog function and wrote tests for it Ex 4.5 helper functions and unit tests step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -7,7 +7,20 @@ const totalLikes = (blogs) => {
     .reduce((likesSum, blog) => (blog.likes + likesSum), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs
+    .reduce((favorite, blog) => {
+      if (favorite.likes === undefined) return blog
+      return blog.likes > favorite.likes
+        ? blog
+        : favorite
+    }, {})
+  const {_id, url, __v, ...result } = favorite
+  return result
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
