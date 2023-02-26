5fd7bdc45ebaaa104e22d4b04b63765178bc28d0
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-02-26 15:47:48 -0500

    Add totalLikes test Ex 4.4 helper functions and unit tests step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs
+    .reduce((likesSum, blog) => (blog.likes + likesSum), 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
