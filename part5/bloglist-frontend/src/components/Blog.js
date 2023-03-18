c1e47b086bc7e05057e7dcfcdb29c7767a647a87
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-03-18 10:09:19 -0400

    add functionality to expand each blog to see more info, and fix bug where authorization header was not sent with requests after refreshing page Ex 5.7 bloglist frontend step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  const blogStyle = {
+    border: '1px solid',
+    padding: 5
+  }
+
+  if (expanded) return (
+    <div style={blogStyle}>
+      <div>
+      {blog.title} {blog.author} <button onClick={toggleExpanded}>hide</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>
+        {blog.user.name}
+      </div>
+      
+    </div>  
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleExpanded}>view</button>
+    </div>  
+  )
+}
 
 export default Blog
