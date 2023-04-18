6befdb47c691f8a912a7ada145cc9ef0d9025ba0
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-04-17 21:34:33 -0400

    write test checking that Blog's url and likes are shown after the view button is clicked Ex 5.14 bloglist tests step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author, not url or likes by default', () => {
-  const blog = {
-    title: 'a blog',
-    author: 'a blogger',
-    url: 'http://blog.blog.com',
-    likes: 42
-  }
+describe('<Blog />', () => {
+  let container
 
-  const { container } = render(<Blog blog={blog} />)
-  const url = container.querySelector('.url')
-  const likes = container.querySelector('.likes')
-  const titleAndAuthor = screen.getByText('a blog a blogger')
+  beforeEach(() => {
+    const blog = {
+      title: 'a blog',
+      author: 'a blogger',
+      url: 'http://blog.blog.com',
+      likes: 42,
+      user: {
+        name: 'test user'
+      }
+    }
 
-  expect(titleAndAuthor).toBeDefined()
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
+    container = render(<Blog blog={blog} />).container
+  })
+
+  test('renders title and author, not url or likes by default', () => {
+    const url = container.querySelector('.url')
+    const likes = container.querySelector('.likes')
+    const titleAndAuthor = screen.getByText('a blog a blogger')
+
+    expect(titleAndAuthor).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
+
+  test('renders URL and number of likes after expand button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText('http://blog.blog.com')
+    const likes = screen.getByText('likes 42')
+    const titleAndAuthor = screen.getByText('a blog a blogger')
+
+    expect(titleAndAuthor).toBeDefined()
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+  })
 })
