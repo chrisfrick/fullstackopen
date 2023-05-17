3526783536bcbfd3fe90a0df5a738b42c6979d87
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-05-17 14:59:43 -0400

    write tests for successful and unsuccessful logins Ex 5.18 bloglist e2e testing step 2

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'tester',
+      password: 'secret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,26 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+    it('fails with incorrect credentials', function() {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('incorrect')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'incorrect username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Test User logged in')
+    })
   })
 })
