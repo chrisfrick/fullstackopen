88921d63fd5cd03d3c38ccf6f02c7f538c25f2b9
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-06-01 20:44:08 -0400

    change reset to onReset to fix invalid 'reset' prop on <input> tag Ex 7.6 anecdotes and hooks step3

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -6,8 +6,7 @@ export const useField = (type) => {
   const onChange = (event) => {
     setValue(event.target.value)
   }
-
-  const reset = () => {
+  const onReset = () => {
     setValue('')
   }
 
@@ -15,6 +14,6 @@ export const useField = (type) => {
     type,
     value,
     onChange,
-    reset
+    onReset
   }
 }
