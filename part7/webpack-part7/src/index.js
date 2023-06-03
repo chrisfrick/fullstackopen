9547cd18dfd942e40ee54256e71364ab86a06966
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-06-03 17:37:53 -0400

    add index.css

--- part7/webpack-part7/src/index.js
+++ part7/webpack-part7/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'core-js/stable/index.js'
 import 'regenerator-runtime/runtime.js'
 import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
