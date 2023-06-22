74ed2be0d4794cfe4a4b44c558d0a069b6d6dcd7
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-06-22 16:58:25 -0400

    graphql phonebook exercieses, library-frontend initial commit, library-frontent implement authors view Ex 8.8 authors view

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,12 +1,8 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  gql,
-} from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
